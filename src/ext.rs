778abdfabbefd45f38420cba6bf0cb13b2c9f935
Zachary Tong <zacharyjtong@gmail.com>
2015-11-21 09:56:15 -0500

    Add netbsd to conditional compilation for keepalive

--- src/ext.rs
+++ src/ext.rs
@@ -542,7 +542,7 @@ impl<T: AsRawSocket> AsSock for T {
 cfg_if! {
     if #[cfg(any(target_os = "macos", target_os = "ios"))] {
         use libc::TCP_KEEPALIVE as KEEPALIVE_OPTION;
-    } else if #[cfg(target_os="openbsd")] {
+    } else if #[cfg(any(target_os = "openbsd", target_os = "netbsd"))] {
         use libc::SO_KEEPALIVE as KEEPALIVE_OPTION;
     } else if #[cfg(unix)] {
         use libc::TCP_KEEPIDLE as KEEPALIVE_OPTION;
