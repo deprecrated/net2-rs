24f41cbb7368a47a7c536cd8ec0667ad98aef301
Patrick Mooney <pmooney@pfmooney.com>
2020-04-14 02:54:20 +0000

    Add support for illumos target

--- src/ext.rs
+++ src/ext.rs
@@ -28,6 +28,7 @@ cfg_if! {
                  target_os = "netbsd",
                  target_os = "openbsd",
                  target_os = "solaris",
+                 target_os = "illumos",
                  target_env = "uclibc"))] {
         use libc::IPV6_JOIN_GROUP as IPV6_ADD_MEMBERSHIP;
         use libc::IPV6_LEAVE_GROUP as IPV6_DROP_MEMBERSHIP;
